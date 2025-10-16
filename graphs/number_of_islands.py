a48e9c22b3c0a195f7376f942107675e167ba008
maarisaay <maarisaay@gmail.com>
2025-10-16 22:02:46 +0200

    Number of islands task

---
+++ graphs/number_of_islands.py
@@ -0,0 +1,39 @@
+"""
+LeetCode - Number of Islands
+Link: https://leetcode.com/problems/number-of-islands/description/
+
+Problem:
+Given an m x n 2D binary grid grid which represents a map of '1's (land) and '0's (water), return the number of islands.
+
+An island is surrounded by water and is formed by connecting adjacent lands horizontally or vertically. You may assume all four edges of the grid are all surrounded by water.
+"""
+
+from typing import List
+
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        if not grid:
+            return 0
+
+        rows, cols = len(grid), len(grid[0])
+        islands = 0
+
+        def dfs(row, col):
+            if row < 0 or col < 0 or row >= rows or col >= cols or grid[row][col] == "0":
+                return
+            grid[row][col] = "0"
+
+            # check four directions
+            dfs(row + 1, col)
+            dfs(row - 1, col)
+            dfs(row, col + 1)
+            dfs(row, col - 1)
+
+        for row in range(rows):
+            for col in range(cols):
+                if grid[row][col] == "1":
+                    islands += 1
+                    dfs(row, col)
+
+        return islands
+
